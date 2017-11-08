9aac103caee0469066634552a1b63519b0e39032
Timur Powenda <timur.powenda@live.de>
2017-11-08 22:08:38 +0100

    Added MapVC

--- ScrollView Excercise/ViewController.swift
+++ ScrollView Excercise/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let mapVC = MapViewController()
+        print(mapVC)
     }
 
     override func didReceiveMemoryWarning() {
